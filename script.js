5eb01f0b83554ac3f522f6e0a65ac58a77d24e79
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-10-19 21:16:32 -0300

    Responsividade e ajustes para facilitar a leitura

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-console.log("Primeira versão")
-const myObserver = new IntersectionObserver((entries) =>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        }
-        else{
-            entry.target.classList.remove('show')
-        }
-        console.log(entries)
-    })
-})
-const elements = document.querySelectorAll('.hidden')
+// console.log("Primeira versão")
+// const myObserver = new IntersectionObserver((entries) =>{
+//     entries.forEach((entry)=>{
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show')
+//         }
+//         else{
+//             entry.target.classList.remove('show')
+//         }
+//         console.log(entries)
+//     })
+// })
+// const elements = document.querySelectorAll('.hidden')
 
-elements.forEach(element => myObserver.observe(element))
+// elements.forEach(element => myObserver.observe(element))
