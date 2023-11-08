403989b6263957b0891690424dba28eccda95140
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-11-08 20:46:40 -0300

    Entre em contado made by GustavoEric

--- script.js
+++ script.js
@@ -13,36 +13,36 @@
 // const elements = document.querySelectorAll('.hidden')
 
 // elements.forEach(element => myObserver.observe(element))
-    var updateButton = document.getElementById("updateDetails");
-    var cancelButton = document.getElementById("cancel");
-    var favDialog = document.getElementById("favDialog");
-    var btCep = document.getElementById("btnCep");
-  
-    // O botão Update abre uma Dialog
-    updateButton.addEventListener("click", function () {
-      favDialog.showModal();
-    });
-    btCep.addEventListener("click",function(){
-        let txtCep = document.getElementById("textCep")
-        let url = `https://viacep.com.br/ws/${txtCep.value}/json/`
-        var rua = document.getElementById("lblRua");
-        var bairro = document.getElementById("lblBairro");
-        var cidade = document.getElementById("lblCidade");
-        var estado = document.getElementById("lblEstado");
+var updateButton = document.getElementById("updateDetails");
+var cancelButton = document.getElementById("cancel");
+var favDialog = document.getElementById("favDialog");
+var btCep = document.getElementById("btnCep");
 
-        alert(txtCep.value)
-        fetch(url).then(function(response){
-            response.json().then(function(data){
-                rua.innerText = data.logradouro
-                bairro.innerText = data.bairro
-                cidade.innerText = data.localidade
-                estado.innerText = data.uf
-                console.log(data)
-            });
+// O botão Update abre uma Dialog
+updateButton.addEventListener("click", function () {
+  favDialog.showModal();
+});
+btCep.addEventListener("click",function(){
+    let txtCep = document.getElementById("textCep")
+    let url = `https://viacep.com.br/ws/${txtCep.value}/json/`
+    var rua = document.getElementById("lblRua");
+    var bairro = document.getElementById("lblBairro");
+    var cidade = document.getElementById("lblCidade");
+    var estado = document.getElementById("lblEstado");
+
+    alert(txtCep.value)
+    fetch(url).then(function(response){
+        response.json().then(function(data){
+            rua.innerText = data.logradouro
+            bairro.innerText = data.bairro
+            cidade.innerText = data.localidade
+            estado.innerText = data.uf
+            console.log(data)
         });
     });
-  
-    // O botão cancelButtom fecha uma Dialog
-    cancelButton.addEventListener("click", function () {
-      favDialog.close();
-    });
+});
+
+// O botão cancelButtom fecha uma Dialog
+cancelButton.addEventListener("click", function () {
+  favDialog.close();
+});
