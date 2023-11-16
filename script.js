ecce5fa9142f81d97199392ff7a523da4807e128
Gustavo Eric Alves Melo <gugaben28@gmail.com>
2023-11-15 21:20:41 -0300

    update

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
-// console.log("Primeira versão")
-// const myObserver = new IntersectionObserver((entries) =>{
-//     entries.forEach((entry)=>{
-//         if(entry.isIntersecting){
-//             entry.target.classList.add('show')
-//         }
-//         else{
-//             entry.target.classList.remove('show')
-//         }
-//         console.log(entries)
-//     })
-// })
-// const elements = document.querySelectorAll('.hidden')
+var entreEmContato = document.getElementById("botao_contato_dialog");
+var botaoCancelar = document.getElementById("cancelar");
+botaoConfirmar = document.getElementById("confirmar")
+var dialog_contato = document.getElementById("dialog_contato");
+var botao_cep = document.getElementById("botao_cep");
 
-// elements.forEach(element => myObserver.observe(element))
-    var updateButton = document.getElementById("updateDetails");
-    var cancelButton = document.getElementById("cancel");
-    var favDialog = document.getElementById("favDialog");
-    var btCep = document.getElementById("btnCep");
-  
-    // O botão Update abre uma Dialog
-    updateButton.addEventListener("click", function () {
-      favDialog.showModal();
-    });
-    btCep.addEventListener("click",function(){
-        let txtCep = document.getElementById("textCep")
-        let url = `https://viacep.com.br/ws/${txtCep.value}/json/`
-        var rua = document.getElementById("lblRua");
-        var bairro = document.getElementById("lblBairro");
-        var cidade = document.getElementById("lblCidade");
-        var estado = document.getElementById("lblEstado");
+var rua = document.getElementById("label_rua");
+var bairro = document.getElementById("label_bairro");
+var cidade = document.getElementById("label_cidade");
+var estado = document.getElementById("label_estado");
 
-        alert(txtCep.value)
-        fetch(url).then(function(response){
-            response.json().then(function(data){
-                rua.innerText = data.logradouro
-                bairro.innerText = data.bairro
-                cidade.innerText = data.localidade
-                estado.innerText = data.uf
-                console.log(data)
-            });
+// A var 'entreEmContato' abre uma Dialog (quase como um pop-up)
+entreEmContato.addEventListener("click", function () {
+    dialog_contato.showModal();
+});
+botao_cep.addEventListener("click", function () {
+    let cep_usuario = document.getElementById("cep_usuario") // Cep Digitado pelo usuário
+    let url = `https://viacep.com.br/ws/${cep_usuario.value}/json/` // Valida o CEP digitado
+    fetch(url)
+    .then(function (response) { // Que que é fetch?? Then?????
+        response.json()
+        .then(function (data) { // Reponder Jonshon? que isso?
+            rua.innerText = data.logradouro
+            bairro.innerText = data.bairro // pra que serve isso tudo?
+            cidade.innerText = data.localidade
+            estado.innerText = data.uf
+            console.log(data) // daonde vem essa data?
         });
     });
-  
-    // O botão cancelButtom fecha uma Dialog
-    cancelButton.addEventListener("click", function () {
-      favDialog.close();
-    });
+});
+
+// O botão 'botaocancelar' fecha a Dialog
+botaoCancelar.addEventListener("click", function () {
+    dialog_contato.close();
+});
+botaoConfirmar.addEventListener("click", function () {
+    
+// https://github.com/github/fetch
+fetch("https://formsubmit.co/ajax/gugaben28@gmail.com", {
+    method: "POST",
+    headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        Rua: rua.innerText,
+        Bairro: bairro.innerText, // pra que serve isso tudo?
+        Cidade: cidade.innerText,
+        Estado: estado.innerText 
+    })
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+    alert(rua.innerText)
+        
+});
