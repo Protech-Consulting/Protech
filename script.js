f233df515f3be4e5fdc28a145f3b067454f34879
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-11-16 19:49:01 -0300

    Entre em contato funcionando

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ botaoConfirmar = document.getElementById("confirmar")
 var dialog_contato = document.getElementById("dialog_contato");
 var botao_cep = document.getElementById("botao_cep");
 
+var nome = document.querySelector("#nome_para_contato");
 var rua = document.getElementById("label_rua");
 var bairro = document.getElementById("label_bairro");
 var cidade = document.getElementById("label_cidade");
 var estado = document.getElementById("label_estado");
+
 
 // A var 'entreEmContato' abre uma Dialog (quase como um pop-up)
 entreEmContato.addEventListener("click", function () {
@@ -17,14 +19,14 @@ botao_cep.addEventListener("click", function () {
     let cep_usuario = document.getElementById("cep_usuario") // Cep Digitado pelo usuário
     let url = `https://viacep.com.br/ws/${cep_usuario.value}/json/` // Valida o CEP digitado
     fetch(url)
-    .then(function (response) { // Que que é fetch?? Then?????
-        response.json()
-        .then(function (data) { // Reponder Jonshon? que isso?
+    .then(function (response) { // fetch é . . .
+        response.json() // json é . . .
+        .then(function (data) {
             rua.innerText = data.logradouro
-            bairro.innerText = data.bairro // pra que serve isso tudo?
+            bairro.innerText = data.bairro
             cidade.innerText = data.localidade
             estado.innerText = data.uf
-            console.log(data) // daonde vem essa data?
+            console.log(data)
         });
     });
 });
@@ -32,9 +34,14 @@ botao_cep.addEventListener("click", function () {
 // O botão 'botaocancelar' fecha a Dialog
 botaoCancelar.addEventListener("click", function () {
     dialog_contato.close();
+    rua.innerText = ""
+    bairro.innerText = ""
+    cidade.innerText = ""
+    estado.innerText = ""
 });
 botaoConfirmar.addEventListener("click", function () {
     
+
 fetch("https://formsubmit.co/ajax/c7b114120c276af9575414a9d94a78ba", {
     method: "POST",
     headers: { 
@@ -42,6 +49,7 @@ fetch("https://formsubmit.co/ajax/c7b114120c276af9575414a9d94a78ba", {
         'Accept': 'application/json'
     },
     body: JSON.stringify({
+        Nome: nome.value,
         Rua: rua.innerText,
         Bairro: bairro.innerText,
         Cidade: cidade.innerText,
