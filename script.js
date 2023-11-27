90cee95b7edb640061db791bcdb89072900d1795
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-11-27 19:47:04 -0300

    Entre em contato à prova de usuários!

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var entreEmContato = document.getElementById("botao_contato_dialog");
+var dialog_contato = document.getElementById("dialog_contato");
+
 var botaoCancelar = document.getElementById("cancelar");
 var botaoConfirmar = document.getElementById("confirmar")
-var dialog_contato = document.getElementById("dialog_contato");
 var botao_checar_cep = document.getElementById("botao_checar_cep");
 
 var nome = document.querySelector("#nome_para_contato");
+var cep_usuario = document.getElementById("cep_usuario")
+
 var rua = document.getElementById("label_rua");
 var bairro = document.getElementById("label_bairro");
 var cidade = document.getElementById("label_cidade");
@@ -17,8 +20,26 @@ entreEmContato.addEventListener("click", function () {
 });
 
 
+// Botão CHECAR
 botao_checar_cep.addEventListener("click", function () {
-    let cep_usuario = document.getElementById("cep_usuario"); // Cep Digitado pelo usuário
+    cep_usuario = document.getElementById("cep_usuario"); // Cep Digitado pelo usuário
+    if(cep_usuario.value == "") {
+        rua.innerHTML = "Insira um CEP válido!";
+        rua.style.color = "#fc2b2b";
+        rua.style.fontSize = "1.45em";
+        rua.style.marginTop = "20px";
+        bairro.innerHTML = "";
+        cidade.innerHTML = "";
+        estado.innerHTML = "";
+        return;
+    }
+    else {
+        rua.style.marginTop = "5px";
+        rua.style.color = "#f2f2f2";
+        rua.style.textShadow = "2px 2px 3px black";
+        rua.style.fontSize = "1.15em"
+    }
+
     let url = `https://viacep.com.br/ws/${cep_usuario.value}/json/`; // Valida o CEP digitado
     fetch(url)
         .then(function (response) { // fetch é . . .
@@ -46,6 +67,11 @@ botaoCancelar.addEventListener("click", function () {
 
 // Botão CONFIRMAR
 botaoConfirmar.addEventListener("click", function () {
+    if(nome.value == "" || cep_usuario.value == "") { // Validação para ver se o usuário não deixou nada em branco
+        alert("Por favor, insira as informações corretamente!");
+        return;
+    }
+
     fetch("https://formsubmit.co/ajax/c7b114120c276af9575414a9d94a78ba", {
         method: "POST",
         headers: {
